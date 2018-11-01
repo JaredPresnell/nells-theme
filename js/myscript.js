219e9166ac882638c5b260ee5580268bbca91732
JaredPresnell <jared.presnell@gmail.com>
2018-11-01 11:28:12 -0700

    moved contact to plugin

--- js/myscript.js
+++ js/myscript.js
@@ -21,115 +21,60 @@ jQuery(document).ready( function($){
 		}
 	});	
 	
-	//BOOKS
-	//ajax for sorting books
-	// $(document).on('click', '#js-recent', function(e){
-	// 	e.preventDefault();
-	// 	var ajaxurl = my_ajax_object.ajax_url; // from enqueue.php
-	// 	$.ajax({
-	// 		url : my_ajax_object.ajax_url,
-	// 		type : 'post',
-	// 		data : {
-	// 			action : "change_sort_recent"
-	// 		}, 
-	// 		error : function( response ){
-	// 			console.log(response);
-	// 		},
-	// 		success : function( response ){
-	// 			$('.books').html(response);
-	// 		}	
-	// 	});
-	// });
-	// $(document).on('click', '#js-author', function(e){
+	// // CONTACT FORM
+	// $('#nellsContactForm').on('submit', function(e){
 	// 	e.preventDefault();
 	// 	var ajaxurl = my_ajax_object.ajax_url;// from enqueue.php
+	// 	var name = $('#name').val();
+	// 	var email = $('#email').val();
+	// 	var message = $('#message').val();
+
+	// 	if(name=="" || email=="" || message=="")
+	// 	{
+	// 		console.log('fields required~!');	
+	// 		return;
+	// 	}
+	// 	var form = $('#nellsContactForm');
+	// 	form.find('input, button, textarea').attr('disabled','disabled');
+	// 	$('.js-form-submit').addClass('js-show-feedback');
 	// 	$.ajax({
 	// 		url : my_ajax_object.ajax_url,
 	// 		type : 'post',
 	// 		data : {
-	// 			action : "change_sort_author"
+	// 			action : "submit_nells_contact",
+	// 			name : name, 
+	// 			email : email,
+	// 			message : message
 	// 		}, 
 	// 		error : function( response ){
 	// 			console.log(response);
+	// 			setTimeout(function(){
+	// 				$('.js-form-error').addClass('js-show-feedback');
+	// 				$('.js-form-submit').removeClass('js-show-feedback');
+	// 				form.find('input, button, textarea').removeAttr('disabled','disabled');
+	// 			},2000);
 	// 		},
 	// 		success : function( response ){
-	// 			$('.books').html(response);
-	// 		}	
-	// 	});
-	// });
-
-	// $(document).on('click', '#js-best-rated', function(e){
-	// 	e.preventDefault();
-	// 	var ajaxurl = my_ajax_object.ajax_url;// from enqueue.php
-	// 	$.ajax({
-	// 		url : my_ajax_object.ajax_url,
-	// 		type : 'post',
-	// 		data : {
-	// 			action : "change_sort_rating"
-	// 		}, 
-	// 		error : function( response ){
 	// 			console.log(response);
-	// 		},
-	// 		success : function( response ){
-	// 			$('.books').html(response);
-	// 		}	
-	// 	});
-	// });
-
-	// CONTACT FORM
-	$('#nellsContactForm').on('submit', function(e){
-		e.preventDefault();
-		var ajaxurl = my_ajax_object.ajax_url;// from enqueue.php
-		var name = $('#name').val();
-		var email = $('#email').val();
-		var message = $('#message').val();
-
-		if(name=="" || email=="" || message=="")
-		{
-			console.log('fields required~!');	
-			return;
-		}
-		var form = $('#nellsContactForm');
-		form.find('input, button, textarea').attr('disabled','disabled');
-		$('.js-form-submit').addClass('js-show-feedback');
-		$.ajax({
-			url : my_ajax_object.ajax_url,
-			type : 'post',
-			data : {
-				action : "submit_nells_contact",
-				name : name, 
-				email : email,
-				message : message
-			}, 
-			error : function( response ){
-				console.log(response);
-				setTimeout(function(){
-					$('.js-form-error').addClass('js-show-feedback');
-					$('.js-form-submit').removeClass('js-show-feedback');
-					form.find('input, button, textarea').removeAttr('disabled','disabled');
-				},2000);
-			},
-			success : function( response ){
-				console.log(response);
-				if(response ==0)
-				{
-					setTimeout(function(){
-						$('.js-form-error').addClass('js-show-feedback');
-						$('.js-form-submit').removeClass('js-show-feedback');
-						form.find('input, button, textarea').removeAttr('disabled','disabled');
-					},2000);
-				}
-				else{
-					setTimeout(function(){
-						$('.js-form-success').addClass('js-show-feedback');
-						$('.js-form-submit').removeClass('js-show-feedback');
-						form.find('input, button, textarea').removeAttr('disabled','disabled').val('');
-					},2000);
-				}
+	// 			if(response ==0)
+	// 			{
+	// 				setTimeout(function(){
+	// 					$('.js-form-error').addClass('js-show-feedback');
+	// 					$('.js-form-submit').removeClass('js-show-feedback');
+	// 					form.find('input, button, textarea').removeAttr('disabled','disabled');
+	// 				},2000);
+	// 			}
+	// 			else{
+	// 				setTimeout(function(){
+	// 					$('.js-form-success').addClass('js-show-feedback');
+	// 					$('.js-form-submit').removeClass('js-show-feedback');
+	// 					form.find('input, button, textarea').removeAttr('disabled','disabled').val('');
+	// 				},2000);
+	// 			}
 				
-			}	
-		});
-	});
+	// 		}	
+	// 	});
+	// });
 	
 });
 
