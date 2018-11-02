3be5ccaab8e4a1d4c178d78c1305743d498b28b1
Jared Presnell <jared.presnell@gmail.com>
2018-11-02 16:11:49 +0000

    php errors

--- functions.php
+++ functions.php
@@ -5,8 +5,5 @@ require get_template_directory() . '/inc/theme-support.php';  	//functions for t
 require get_template_directory() . '/inc/walker.php';			// custom walker functions
 require get_template_directory() . '/inc/add-homepage-class.php'; //adds a custom class to the homepage body
 require get_template_directory() . '/inc/widgets.php'; 
-require get_template_directory() . '/inc/ajax.php'; 
 require get_template_directory() . '/inc/shortcodes.php'; 
-require get_template_directory() . '/inc/custom-post-types.php'; 
-//require get_template_directory() . '/inc/custom-taxonomies.php'; 
 
